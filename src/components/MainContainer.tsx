376209e45fac68946cf328e850988c7b8157db18
Echa Apriliyanto <apriliyantoecha1@gmail.com>
2024-02-29 21:24:31 +0700

    Complete basic layout on 'Projects' and 'Contact' page's

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -4,7 +4,7 @@ export default function MainContainer({
   children: React.ReactNode;
 }) {
   return (
-    <main className='grid md:w-[52vw] mt-[min(12vh,150px)] md:p-5'>
+    <main className='grid md:w-[52vw] mt-[min(12vh,150px)] md:p-5 border'>
       {children}
     </main>
   );
